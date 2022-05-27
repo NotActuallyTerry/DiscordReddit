3ae50c626196d345efcf48fe92fbc327e4cfb6d7
Ike Johnson-Woods <contact@ike.id.au>
2022-05-27 15:08:24 +0800

    Rewrite from near-scratch Fuck you it's all one commit

--- go.mod
+++ go.mod
@@ -1,13 +1,8 @@
-module github.com/dualmoon/discordreddit
+module github.com/NotActuallyTerry/DiscordReddit
 
 go 1.16
 
 require (
-	github.com/BurntSushi/toml v1.1.0
-	github.com/beefsack/go-rate v0.0.0-20220214233405-116f4ca011a0 // indirect
-	github.com/davecgh/go-spew v1.1.1
-	github.com/google/go-querystring v1.1.0 // indirect
-	github.com/jzelinskie/geddit v0.0.0-20200521013404-78c28c13fba2
-	golang.org/x/net v0.0.0-20220524220425-1d687d428aca // indirect
-	golang.org/x/oauth2 v0.0.0-20220524215830-622c5d57e401 // indirect
+	github.com/buger/jsonparser v1.1.1
+	github.com/turnage/graw v0.0.0-20201204201853-a177df1b5c91
 )
---
+++ rewrite.go
@@ -0,0 +1,214 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/buger/jsonparser"
+	"github.com/turnage/graw"
+	"github.com/turnage/graw/reddit"
+)
+
+type announcer struct{}
+
+type CliArgs struct {
+	DiscordWebhookURL string
+	Subreddit         string
+}
+
+type WebhookData struct {
+	Embeds          []Embed `json:"embeds"`
+	DiscordUsername string  `json:"username"`
+	DiscordAvatar   string  `json:"avatar_url"`
+}
+type Embed struct {
+	Title       string      `json:"title"`
+	URL         string      `json:"url"`
+	Color       string      `json:"color"`
+	Description string      `json:"description,omitempty"`
+	Author      EmbedAuthor `json:"author"`
+	Timestamp   string      `json:"timestamp"`
+	Image       EmbedImage  `json:"image,omitempty"`
+}
+type EmbedAuthor struct {
+	Name   string `json:"name"`
+	URL    string `json:"url"`
+	Avatar string `json:"icon_url"`
+}
+type EmbedImage struct {
+	ImageURL string `json:"url,omitempty"`
+}
+
+func grabRemoteJson(url string) ([]byte, error) {
+	httpClient := &http.Client{}
+	var jsonByte []byte
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("could not fetch author's about.json: %s", err)
+	}
+
+	req.Header.Set("User-Agent", userAgent)
+
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error in HTTP client: %s", err)
+	}
+
+	defer resp.Body.Close()
+
+	jsonByte, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("could not byterize author's about.json: %s", err)
+	}
+	return jsonByte, nil
+}
+
+func getAuthorAvatar(username string) string {
+	defaultAvatarUrl := fmt.Sprintf("https://www.redditstatic.com/avatars/defaults/v2/avatar_default_%d.png", rand.Intn(7))
+	authorAboutJson, err := grabRemoteJson(fmt.Sprintf("https://reddit.com/user/%s/about.json", username))
+	if err != nil {
+		log.Printf("Could not fetch JSON: %s", err)
+		return defaultAvatarUrl
+	}
+
+	authorAvatar, err := jsonparser.GetString(authorAboutJson, "data", "icon_img")
+	if err != nil {
+		log.Printf("Failed to parse json: %s", err)
+		return defaultAvatarUrl
+	}
+
+	authorAvatar = strings.Replace(authorAvatar, "&amp;", "&", -1)
+
+	if authorAvatar != "" {
+		return authorAvatar
+	} else {
+		log.Print("Using default avatar")
+		return defaultAvatarUrl
+	}
+}
+
+func populateWebhook(post *reddit.Post) WebhookData {
+	var WebhookContents WebhookData
+	var authorAvatar = getAuthorAvatar(post.Author)
+	if post.IsSelf {
+		WebhookContents = WebhookData{Embeds: []Embed{
+			{
+				Title:       post.Title,
+				URL:         post.URL,
+				Color:       "16729344",
+				Description: post.SelfText,
+				Timestamp:   time.Unix(int64(post.CreatedUTC), 0).Format(time.RFC3339),
+				Author: EmbedAuthor{
+					Name:   post.Author,
+					URL:    fmt.Sprintf("https://reddit.com/user/%s/", post.Author),
+					Avatar: authorAvatar,
+				},
+			}}}
+	} else {
+		WebhookContents = WebhookData{Embeds: []Embed{
+			{
+				Title:     post.Title,
+				URL:       post.URL,
+				Color:     "16729344",
+				Timestamp: time.Unix(int64(post.CreatedUTC), 0).Format(time.RFC3339),
+				Author: EmbedAuthor{
+					Name:   post.Author,
+					URL:    fmt.Sprintf("https://reddit.com/user/%s/", post.Author),
+					Avatar: authorAvatar,
+				},
+				Image: EmbedImage{
+					ImageURL: post.Thumbnail,
+				},
+			}}}
+	}
+	return WebhookContents
+}
+
+func parseArgs() CliArgs {
+	var args CliArgs
+
+	// Build args and parse them
+	flag.StringVar(&args.DiscordWebhookURL, "webhook", "", "Discord Webhook URL")
+	flag.StringVar(&args.Subreddit, "subreddit", "", "Subreddit to monitor")
+	flag.Parse()
+
+	// Set defaults
+	if args.DiscordWebhookURL == "" {
+		log.Fatalf("Webhook not supplied, use --help for info")
+	}
+
+	if args.Subreddit == "" {
+		log.Fatalf("Subreddit not supplied, use --help for info")
+	}
+
+	return args
+}
+
+var args = parseArgs()
+var userAgent = "Ububu:grabba-da-cake:0.6.9"
+
+func (a *announcer) Post(post *reddit.Post) error {
+	log.Printf("New FullID is: %v, Thumbnail URL is: %s", post.URL, post.Thumbnail)
+
+	// Create json byte data for body
+	WebhookJSON, err := json.Marshal(populateWebhook(post))
+	if err != nil {
+		log.Printf("Error marshalling JSON data: %v", err)
+	}
+
+	// POST to Discord
+	response, err := http.Post(args.DiscordWebhookURL, "application/json", bytes.NewBuffer(WebhookJSON))
+	if err != nil {
+		log.Printf("Error in HTTP POST to Discord: %v", err)
+	}
+	body_byte, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Printf("Error from Discord after HTTP POST: %v", err)
+	}
+
+	// Print POST response
+	log.Printf(`%s posted "%s"`, post.Author, post.Title)
+	log.Printf("Webhook response: %s", body_byte)
+	return nil
+}
+
+func main() {
+	// Get an api handle to reddit for a logged out (script) program,
+	// which forwards this user agent on all requests and issues a request at
+	// most every 5 seconds.
+	apiHandle, err := reddit.NewScript(userAgent, time.Second*10)
+
+	if err != nil {
+		log.Fatalf("Failed to create Script: %v", err)
+	}
+
+	// Create a configuration specifying what event sources on Reddit graw
+	// should connect to the bot.
+	cfg := graw.Config{Subreddits: []string{args.Subreddit}}
+
+	log.Printf("Listening to /r/%s", args.Subreddit)
+
+	// launch a graw scan in a goroutine using the bot, handle, and config. The
+	// returned "stop" and "wait" are functions. "stop" will stop the graw run
+	// at any time, and "wait" will block until it finishes.
+	_, wait, err := graw.Scan(&announcer{}, apiHandle, cfg)
+
+	if err != nil {
+		log.Printf("Graw error: %v", err)
+	}
+
+	// This time, let's block so the bot will announce (ideally) forever.
+	if err := wait(); err != nil {
+		log.Printf("graw run encountered an error: %v", err)
+	}
+
+}
